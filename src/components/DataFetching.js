1b85488c616dec115c8bd65b23e80c5ac3d83057
kr-nirmit <nirmit.t@knackroot.com>
2022-01-12 18:50:10 +0530

    Fetch data click button

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -4,9 +4,10 @@ import React,{useState, useEffect} from 'react'
 function DataFetching() {
     const [posts, setposts] = useState({})
     const [id, setid] = useState(1)
+    const [idButton, setidButton] = useState(1)
 
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idButton}`)
         .then(res => {
             console.log(res);
             setposts(res.data)
@@ -18,12 +19,17 @@ function DataFetching() {
         return () => {
             
         }
-    }, [id])
+    }, [idButton])
+
+    const handleClick = () => {
+        setidButton(id)
+    } 
 
     return (
         <div>
             <h1>List of Posts <br />GET API Call</h1>
             <input type='text' value={id} onChange={e => setid(e.target.value)}></input>
+            <button onClick={handleClick}> Fectch Post </button>
             <div>{posts.title}</div>
                 {/* <table border='1'>
                     <tr>
