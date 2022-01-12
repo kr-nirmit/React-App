329edd01fb90d6b480cc39ac531cdaef91bc9771
kr-nirmit <nirmit.t@knackroot.com>
2022-01-12 12:53:41 +0530

    Add CSS in HTTP GET request

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -27,15 +27,34 @@ class PostList extends Component {
         const { posts, errorMsg } = this.state
         return (
             <div>
-                List of Posts 
-                {posts.map((post) => <div key={post.id}>{post.id}. {post.title}</div>)}
+                {/* List of Posts  */}
+                {/* {posts.map((post) => <div key={post.id}>{post.id}. {post.title}</div>)} */}
                 {/* {
                     posts.length ?
                     posts.map((post) => {<div key={post.id}>{post.title}</div>}) :
                     null
                 } */}
                 { errorMsg ? <div>{errorMsg}</div> : null }
+
+                <h1>List of Posts <br />GET API Call</h1>
+                <table border='1'>
+                    <tr>
+                        <td>ID</td>
+                        <td>Title Name</td>
+                        <td>Body</td>
+                    </tr>
+                        {
+                            posts.map((item) => 
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.title}</td>
+                                <td>{item.body}</td>
+                            </tr>
+                            )
+                        }
+                </table>
             </div>
+
         )
     }
 }
