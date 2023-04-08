b9d2fefe48befc2174e02acee59b57295fcb41b4
kr-nirmit <nirmit.t@knackroot.com>
2023-04-08 17:58:30 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import HookCounter from './components/HookCounter';
-import UseStateObject from './components/UseStateObject';
-import IntervalCounter from './components/IntervalCounter';
+// import PostList from './components/PostList';
+// import PostForm from './components/PostForm';
+// import HookCounter from './components/HookCounter';
+// import UseStateObject from './components/UseStateObject';
+// import IntervalCounter from './components/IntervalCounter';
 import DataFetching from './components/DataFetching';
 
 function App() {
