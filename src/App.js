4b02731d3d7cd32eb52c6eef0a79f28a8f98c099
kr-nirmit <nirmit.t@knackroot.com>
2023-04-10 11:26:01 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-// import logo from './logo.svg';
-import './App.css';
-// import PostList from './components/PostList';
-// import PostForm from './components/PostForm';
-// import HookCounter from './components/HookCounter';
-// import UseStateObject from './components/UseStateObject';
-// import IntervalCounter from './components/IntervalCounter';
-import DataFetching from './components/DataFetching';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <PostList /> */}
-      {/* <PostForm /> */}
-      {/* <HookCounter /> */}
-      {/* <UseStateObject /> */}
-      {/* <IntervalCounter /> */}
-      <DataFetching />
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>
+      This is the home page!
+    </p>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This is the about page!
+    </p>
+  </div>
+)
+
+const Contact = ({ match }) => (
+  <div>
+    <h2>Contact</h2>
+    <p>
+      This is the contact us page!
+    </p>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
     </div>
-  );
-}
-
+  </Router>
+)
 export default App;
